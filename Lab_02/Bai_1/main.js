c1a35aecd86388e499012a09fc449e16675223c3
Đỗ Trọng Khánh <79818873+trong-khanh-1109@users.noreply.github.com>
2021-10-05 22:24:16 +0700

    Delete main.js

--- Lab_02/Bai_1/main.js
+++
@@ -1,6 +0,0 @@
-function calcLuong() {
-    var luong = document.getElementById("input-luong").value;
-    var heSoLuong = document.getElementById("input-hesoluong").value;
-    var totalLuong = Number(luong) * Number(heSoLuong);
-    document.getElementById("total-luong").innerHTML = totalLuong;
-}
